4f2bf47cf54d8c9a48bd0e6cd9bb318bcca2704b
salmateache <salmateache0@gmail.com>
2023-01-18 15:13:20 +0100

    create user migration +editing user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'telephone',
+        'dob',
+        'role',
+        'company',
         'password',
     ];
 
