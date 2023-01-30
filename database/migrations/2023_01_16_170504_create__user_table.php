5ae8716f97f3e6979df4d6ca67ed41701c1b1fc0
AZROUR1998 <marouan.azrour@uha.fr>
2023-01-30 14:44:10 +0100

    adding users migration

---
+++ database/migrations/2023_01_16_170504_create__user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('email')->unique();
+            $table->integer('telephone');
+            $table->date('dob');
+            $table->text('role');
+            $table->text('company');
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_user');
+    }
+}
