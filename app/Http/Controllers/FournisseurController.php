d2b77d20b65278d0ddc1870ab799c7d71e20f4ef
salmateache <salmateache0@gmail.com>
2023-02-12 21:06:27 +0100

    créer les page afficher fournisseur et historique

--- app/Http/Controllers/FournisseurController.php
+++ app/Http/Controllers/FournisseurController.php
@@ -12,9 +12,36 @@ class FournisseurController extends Controller
     public function index()
     {
       
-        return view('historique');
+        return view('addFournisseur');
            
     }
 
+    public function addFournisseur(Request $request)
+    {
+        $adresse=$request->adresse.' '.$request->city.' '. $request->pays; 
+        $fournisseur = new Fournisseur;
+        $fournisseur->name = $request->name;
+        $fournisseur->adresse =$adresse;
+    
+        $fournisseur->telephone = $request->telephone;
+        $fournisseur->added_by=auth()->user();
+
+        $fournisseur->save();
+        
+       
+        $historique= new Historique;
+        $historique->title='fournisseur ajouté';
+        $historique->description='un fournisseur a été ajouté par :'.$fournisseur->added_by;
+        $historique->seen=false;
+        $historique->save();
  
+        return redirect('addFournisseur')->with('status', ' le fournisseur a été ajouté ');
+    }
+    public function afficherFournisseur()
+    {
+        $fournisseurs= Fournisseur::all();
+        
+        return view('afficherFournisseur',['fournisseurs'=>$fournisseurs]);
+       
+    }
 }
